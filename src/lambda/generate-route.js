01969c8b20a56dff4743617315af0c6a00e37f87
joel weber <joel2000.jw@gmail.com>
2018-06-09 17:42:04 +0200

    Added custom code, code checking and duplicate checking

--- src/lambda/generate-route.js
+++ src/lambda/generate-route.js
@@ -24,65 +24,114 @@ export function handler(event, context, callback) {
         // look for this code in our stash
         if (!err && response.statusCode === 200) {
             var routes = JSON.parse(body);
-
-            for (var item in routes) {
-                // return the result when we find the match
-                if (routes[item].data.destination == destination) {
-                    console.log("We searched for " + routes[item].data.destination + " and we found " + routes[item].data.code);
-                    var code = routes[item].data.code;
-                    // tell the user what their shortcode will be
-                    return callback(null, {
-                        statusCode: 200,
-                        headers: {
-                            "Content-Type": "application/json"
-                        },
-                        body: JSON.stringify({
-                            "url": rootURL + code
-                        })
-                    });
-                    console.log(code);
-                } else if (routes[item].data.code == precode) {
-                    console.log("We searched for " + code + " and we found " + routes[item].data.destination);
-                } else {
-                    if (!precode) {
-                        // generate a unique short code (stupidly for now)
-                        var hash = new Hashids();
-                        var number = Math.round(new Date().getTime() / 100);
-                        var code = hash.encode(number);
-                        console.log(code)
-                    }
-                    // prepare a payload to post
-                    var payload = {
-                        'form-name': "routes",
-                        'destination': destination,
-                        'code': code,
-                        'expires': ""
-                    };
-
-                    // post the new route to the Routes form
-                    request.post({
-                        'url': rootURL,
-                        'formData': payload
-                    }, function(err, httpResponse, body) {
-                        var msg;
-                        if (err) {
-                            msg = "Post to Routes stash failed: " + err;
-                        } else {
-                            msg = "Route registered. Site deploying to include it. " + rootURL + code
+            if(routes.length != 0) {
+                for(var item in routes) {
+                    // return the result when we find the match
+                    if (routes[item].data.destination == destination) {
+                        console.log("We searched for " + routes[item].data.destination + " and we found " + routes[item].data.code);
+                        var code = routes[item].data.code;
+                        // tell the user what their shortcode will be
+                        return callback(null, {
+                            statusCode: 200,
+                            headers: {
+                                "Content-Type": "application/json"
+                            },
+                            body: JSON.stringify({
+                                "url": rootURL + code
+                            })
+                        });
+                        console.log(code);
+                    } else if (routes[item].data.code == precode) {
+                        console.log("We searched for " + precode + " and we found " + routes[item].data.destination);
+						return callback(null, {
+                            statusCode: 200,
+                            headers: {
+                                "Content-Type": "application/json"
+                            },
+                            body: JSON.stringify({
+                                "url": "Error: Code already in use!"
+                            })
+                        });
+                    } else {
+                        if (!precode) {
+                            // generate a unique short code (stupidly for now)
+                            var hash = new Hashids();
+                            var number = Math.round(new Date().getTime() / 100);
+                            var code = hash.encode(number);
+                            console.log(code)
                         }
-                        console.log(msg);
-                    });
-                    // tell the user what their shortcode will be
-                    return callback(null, {
-                        statusCode: 200,
-                        headers: {
-                            "Content-Type": "application/json"
-                        },
-                        body: JSON.stringify({
-                            "url": rootURL + code
-                        })
-                    });
+                        // prepare a payload to post
+                        var payload = {
+                            'form-name': "routes",
+                            'destination': destination,
+                            'code': code,
+                            'expires': ""
+                        };
+
+                        // post the new route to the Routes form
+                        request.post({
+                            'url': rootURL,
+                            'formData': payload
+                        }, function(err, httpResponse, body) {
+                            var msg;
+                            if (err) {
+                                msg = "Post to Routes stash failed: " + err;
+                            } else {
+                                msg = "Route registered. Site deploying to include it. " + rootURL + code
+                            }
+                            console.log(msg);
+                        });
+                        // tell the user what their shortcode will be
+                        return callback(null, {
+                            statusCode: 200,
+                            headers: {
+                                "Content-Type": "application/json"
+                            },
+                            body: JSON.stringify({
+                                "url": rootURL + code
+                            })
+                        });
+                    }
                 }
+            } else {
+                if (!precode) {
+                    // generate a unique short code (stupidly for now)
+                    var hash = new Hashids();
+                    var number = Math.round(new Date().getTime() / 100);
+                    var code = hash.encode(number);
+                    console.log(code)
+                }
+                // prepare a payload to post
+                var payload = {
+                    'form-name': "routes",
+                    'destination': destination,
+                    'code': code,
+                    'expires': ""
+                };
+
+                // post the new route to the Routes form
+                request.post({
+                    'url': rootURL,
+                    'formData': payload
+                }, function(err, httpResponse, body) {
+                    var msg;
+                    if (err) {
+                        msg = "Post to Routes stash failed: " + err;
+                    } else {
+                        msg = "Route registered. Site deploying to include it. " + rootURL + code
+                    }
+                    console.log(msg);
+                });
+                // tell the user what their shortcode will be
+                return callback(null, {
+                    statusCode: 200,
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify({
+                        "url": rootURL + code
+                    })
+                });
             }
         }
     });
