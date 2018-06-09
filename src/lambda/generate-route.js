5c7124e6071395496fe85a8a807397d2278c5b5a
joel weber <joel2000.jw@gmail.com>
2018-06-09 14:14:53 +0200

    Added custom shortcodes and duplicate route checking

--- src/lambda/generate-route.js
+++ src/lambda/generate-route.js
@@ -9,20 +9,51 @@ export function handler(event, context, callback) {
 
   // Set the root URL according to the Netlify site we are within
   var rootURL =  process.env.URL + "/";
+  var url = "https://api.netlify.com/api/v1/forms/" + process.env.ROUTES_FORM_ID + "/submissions/?access_token=" + process.env.API_AUTH;
 
   // get the details of what we are creating
   var destination = event.queryStringParameters['to'];
-
-  // generate a unique short code (stupidly for now)
-  var hash = new Hashids();
-  var number = Math.round(new Date().getTime() / 100);
-  var code = hash.encode(number);
-
+  var code = event.queryStringParameters['code'];
+  
+  if( !code ) {
+	  // generate a unique short code (stupidly for now)
+	  var hash = new Hashids();
+	  var number = Math.round(new Date().getTime() / 100);
+	  var code = hash.encode(number);
+  }
+  
   // ensure that a protocol was provided
   if(destination.indexOf("://") == -1) {
     destination = "http://" + destination;
   }
 
+	request(url, function(err, response, body){
+
+	// look for this code in our stash
+	if(!err && response.statusCode === 200){
+	  var routes = JSON.parse(body);
+
+	  for(var item in routes) {
+		// return the result when we find the match
+		if(routes[item].data.code == code && routes[item].data.url == destination) {
+		  console.log("We searched for " + code + " and we found " + routes[item].data.destination);
+		  return callback(null, {
+			statusCode: 200,
+			headers: {"Content-Type": "application/json"},
+			body: JSON.stringify({url: rootURL + code})
+		  })
+		} else if(routes[item].data.code == code && routes[item].data.url == !destination) {
+		  console.log("We searched for " + code + " and we found " + routes[item].data.destination);
+		  return callback(null, {
+			statusCode: 200,
+			headers: {"Content-Type": "application/json"},
+			body: JSON.stringify({url: "Error: code already exists!"})
+		  })
+		}
+	  }
+	}
+	});
+  
   // prepare a payload to post
   var payload = {
     'form-name' : "routes",
